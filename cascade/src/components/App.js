cd288ae6a716e6e51de24fee8ba23755969e047c
Keith Silcock <silcockk@gmail.com>
2018-09-22 11:00:20 -0700

    created react app

---
+++ cascade/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../assets/CSS/App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">Hellow</div>;
+  }
+}
+
+export default App;
